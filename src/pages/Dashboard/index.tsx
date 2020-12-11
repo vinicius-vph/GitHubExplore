7c1ccd0efb265aacec55df08bc87f869361a979d
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-11 16:02:49 -0300

    navigate among pages of our application is run

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -72,7 +73,7 @@ const Dashboard: React.FC = () => {
             { inputError && <Error>{inputError}</Error>}
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                         <img 
                         src={repository.owner.avatar_url}
                         alt={repository.owner.login}/>
@@ -82,7 +83,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20}/>
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
