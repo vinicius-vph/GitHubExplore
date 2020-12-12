0d49224d670499e50079b98fd5851b23001f53ee
Vinicius Santos <vinicius.vph@gmail.com>
2020-12-12 10:30:33 -0300

    created repository styles file and styled the repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +12,47 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-return (<h1>Repository:{params.repository}</h1>);
-}
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHubExplorer" />
+                <Link to="/"><FiChevronLeft size={16} /> Voltar </Link>
+            </Header>
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4" alt=""/>
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>descrição</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1890</strong>
+                        <span>stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+            <Issues>
+                <Link  to={`dddddddddd`}>
+                    <div>
+                        <strong>uuuuuuuuu</strong>
+                        <p>iiiiiiiiiiii</p>
+                    </div>
+
+                    <FiChevronRight size={20}/>
+                </Link>
+            </Issues>
+        </>
+    );
+};
 
 export default Repository;
